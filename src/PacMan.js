70028781ac64565aff577bdc9d42bfe425964eb5
Aron-Kaniyarakath-Joseph <ajoseph2148@sdsu.edu>
2025-01-22 16:28:32 -0800

    Creating a Moving PacMan

---
+++ src/PacMan.js
@@ -0,0 +1,78 @@
+import MovingDirection from "./MovingDirection.js";
+
+export default class Pacman {
+    constructor(x, y, tileSize, velocity, tileMap) {
+        this.x = x;
+        this.y = y;
+        this.tileSize = tileSize;
+        this.velocity = velocity;
+        this.tileMap = tileMap;
+
+        this.currentMovingDirection = null;
+        this.requestedMovingDirection = null;
+
+        document.addEventListener("keydown", this.#keydown);
+
+        this.#loadPacmanImages();
+    }
+
+    draw(ctx) {
+        this.#move();
+        ctx.drawImage(this.pacmanImages[this.pacmanImageIndex], this.x, this.y, this.tileSize, this.tileSize);
+    }
+
+    #loadPacmanImages() {
+        const pacmanImage1 = new Image();
+        pacmanImage1.src = "../images/pac0.png";
+
+        const pacmanImage2 = new Image();
+        pacmanImage2.src = "../images/pac1.png";
+
+        const pacmanImage3 = new Image();
+        pacmanImage3.src = "../images/pac2.png";
+
+        const pacmanImage4 = new Image();
+        pacmanImage4.src = "../images/pac1.png";
+
+        this.pacmanImages = [pacmanImage1, pacmanImage2, pacmanImage3, pacmanImage4];
+        this.pacmanImageIndex = 1;
+    }
+
+    #keydown = (event) => {
+        switch (event.keyCode) {
+            case 38: // Up arrow
+                this.requestedMovingDirection = MovingDirection.up;
+                break;
+            case 40: // Down arrow
+                this.requestedMovingDirection = MovingDirection.down;
+                break;
+            case 37: // Left arrow
+                this.requestedMovingDirection = MovingDirection.left;
+                break;
+            case 39: // Right arrow
+                this.requestedMovingDirection = MovingDirection.right;
+                break;
+        }
+    };
+
+    #move() {
+        if (Number.isInteger(this.x / this.tileSize) && Number.isInteger(this.y / this.tileSize)) {
+            this.currentMovingDirection = this.requestedMovingDirection;
+        }
+
+        switch (this.currentMovingDirection) {
+            case MovingDirection.up:
+                this.y -= this.velocity;
+                break;
+            case MovingDirection.down:
+                this.y += this.velocity;
+                break;
+            case MovingDirection.left:
+                this.x -= this.velocity;
+                break;
+            case MovingDirection.right:
+                this.x += this.velocity;
+                break;
+        }
+    }
+}
--- src/TileMap.js
+++ src/TileMap.js
@@ -1,3 +1,5 @@
+import Pacman from "./PacMan.js";
+
 export default class TileMap{
     constructor(tileSize){
         this.tileSize = tileSize;
@@ -5,12 +7,15 @@ export default class TileMap{
         this.yellowDot = new Image();
         this.yellowDot.src = './images/yellowDot.png';
 
+        this.pinkDot = new Image();
+        this.pinkDot.src = "./images/pinkDot.png";
+
         this.wall = new Image();
         this.wall.src = "./images/wall.png";
     }
     map = [
             [1,1,1,1,1,1,1,1,1,1,1,1,1],
-            [1,0,0,0,0,0,0,0,0,0,0,0,1],
+            [1,0,0,0,0,0,0,0,4,0,0,0,1],
             [1,0,1,1,1,1,1,1,0,0,0,0,1],
             [1,0,1,0,0,0,0,0,0,0,1,0,1],
             [1,0,1,0,1,1,1,0,1,0,1,0,1],
@@ -32,11 +37,31 @@ export default class TileMap{
                 if(tile===1){
                     this.#drawWall(ctx, column, row, this.tileSize);
                 }
+                else if(tile === 0){
+                    this.#drawYellowDot(ctx, column, row, this.tileSize);
+                }
+
+                ctx.strokeStyle = "blue";
+                ctx.strokeRect(column*this.tileSize, row*this.tileSize, this.tileSize, this.tileSize)
             }
         }
     }
     #drawWall(ctx, column, row, size ){
         ctx.drawImage(this.wall, column*this.tileSize, row*this.tileSize,size,size );
+    }
+    #drawYellowDot(ctx, column, row, size){
+        ctx.drawImage(this.yellowDot, column*this.tileSize, row*this.tileSize,size,size)
+    }
+    getPacman(velocity){
+        for(let row = 0; row <this.map.length; row++){
+            for(let column=0; column< this.map[row].length; column++){
+                let tile = this.map[row][column];
+                if(tile === 4){
+                    this.map[row][column] = 0;
+                    return new Pacman(column * this.tileSize, row * this.tileSize, this.tileSize, velocity, this);
+                }
+            }
+        }
     }
     setCanvasSize(canvas){
         canvas.width = this.map[0].length * this.tileSize;
